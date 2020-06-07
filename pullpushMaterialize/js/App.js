fa1e7e6924c34df95f2963555b60d0f3f94f9cd1
Sotnas <mr.sansan777@gmail.com>
2020-06-07 17:07:59 -0400

    Todo junto

--- pullpushMaterialize/js/App.js
+++ pullpushMaterialize/js/App.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
-    $('.pushpin-demo-nav').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    $this.pushpin({
-      top: $target.offset().top,
-      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+$(document).ready(function () {
+    $('.pushpin-demo-nav').each(function () {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+            top: $target.offset().top,
+            bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
     });
-  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems,{
+        indicators: true
+    });
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, {
+        height: 300,
+        duration: 250,
+        interval: 1200
+    });
   });
